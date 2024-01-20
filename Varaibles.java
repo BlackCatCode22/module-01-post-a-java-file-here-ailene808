fb960fa155574d7421d408f7cc643ec800e04092
ailene808 <54044885+ailene808@users.noreply.github.com>
2024-01-20 14:06:50 -0800

    Add files via upload

---
+++ Varaibles.java
@@ -0,0 +1,29 @@
+public class Varaibles {
+    public static void main(String[] args) {
+        System.out.println("This is variables and simple math");
+
+        int a = 10;
+        double b = 6.5;
+        double sum = a + b;
+
+        System.out.println("\nThe sum of "+ a +" and " + b + " is " + sum);
+
+        double product = a * b;
+
+        System.out.println("\nThe product of multiplying " + a +" and " + b + " is " + product);
+
+        a = 20;
+        sum = a + b;
+        System.out.println("\nThe new sum of " + a + " and " + b + " is: " + sum + "\n" );
+    }
+
+
+
+
+
+
+
+
+
+
+}
