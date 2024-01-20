0df35fe6e6e5c7688d2f6331a2c6049e76960436
ailene808 <54044885+ailene808@users.noreply.github.com>
2024-01-20 09:09:48 -0800

    Add files via upload

---
+++ Module2_Initials_Exercise.java
@@ -0,0 +1,20 @@
+public class Module2_Initials_Exercise {
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.println("**************************************************************************");
+        System.out.println("**************************************************************************");
+        System.out.println("         ***               *                              ***");
+        System.out.println("        *   *              *                             *   *");
+        System.out.println("       *     *             *                            *     *");
+        System.out.println("      *       *            *                           *       *");
+        System.out.println("     * * * * * *           *                          * * * * * *");
+        System.out.println("    *           *          *                         *           *");
+        System.out.println("   *             *         *                        *             *");
+        System.out.println("  *               *        *                       *               *");
+        System.out.println(" *                 *       * * * * * * * * *      *                 *");
+        System.out.println("**************************************************************************");
+        System.out.println("**************************************************************************");
+
+    }
+}
